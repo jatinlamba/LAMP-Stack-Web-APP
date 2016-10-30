294317879632433b9d72642c79ff8c81ae02445d
jlamba1@hawk.iit.edu <jlamba1@hawk.iit.edu>
2016-10-30 20:37:57 +0000

    Zone added

--- create-app-env.sh
+++ create-app-env.sh
@@ -2,16 +2,13 @@
 
 echo "Creating My aws database"
 
-aws rds create-db-instance \
-    --db-name awsDb \
-    --db-instance-identifier jl-instance1 \
-    --allocated-storage 5 \
-    --db-instance-class db.t2.micro \
-    --engine MySQL \
-    --master-username jatindb \
-    --master-user-password Jlamba1db \
-    --vpc-security-group-ids sg-998e44e0
+## creating database instance
 
+aws rds create-db-instance --db-name awsDb --db-instance-identifier jl-instance1 --allocated-storage 5 --db-instance-class db.t2.micro --engine MySQL --master-username jatindb --master-user-password Jlamba1db --vpc-security-group-ids sg-998e44e0 --availability-zones us-west-2b
+
+echo "Waiting till the instance is available ........................"
+
+## rds wait command till the instance is available"
 aws rds wait db-instance-available --db-instance-identifier jl-instance1
 
-echo "Database is succesfully created"
+echo "Database is succesfully created and Instance is launched successfully"
--- installapp.sh
+++ installapp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "hello" > /home/ubuntu/hello.txt
 sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
 
+git clone git@github.com:illinoistech-itm/jlamba1.git
+
+echo "end"
