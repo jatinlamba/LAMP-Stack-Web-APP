455d4e9af64d04631e02e101a8b034b6ce0e8750
jlamba1@hawk.iit.edu <jlamba1@hawk.iit.edu>
2016-10-30 21:09:07 +0000

    Completed

--- create-app-env.sh
+++ create-app-env.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+securityGrp='sg-998e44e0'
+echo "security group is : $securityGrp"
+
+availabilityZone='us-west-2b'
+echo "availability zone is : $availabilityZone"
+
 echo "Creating My aws database"
 
 ## creating database instance
 
-aws rds create-db-instance --db-name awsDb --db-instance-identifier jl-instance1 --allocated-storage 5 --db-instance-class db.t2.micro --engine MySQL --master-username jatindb --master-user-password Jlamba1db --vpc-security-group-ids sg-998e44e0 --availability-zones us-west-2b
+aws rds create-db-instance \
+    --db-name awsDb \
+    --db-instance-identifier jl-instance1 \
+    --allocated-storage 5 \
+    --db-instance-class db.t2.micro \
+    --engine MySQL \
+    --master-username jatindb \
+    --master-user-password Jlamba1db \
+    --vpc-security-group-ids $securityGrp \
+    --availability-zone $availabilityZone
 
 echo "Waiting till the instance is available ........................"
 
