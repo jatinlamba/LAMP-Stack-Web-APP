02fd2a3784c650eaac2da4f801f2d18fc3fe4bbf
jlamba1@hawk.iit.edu <jlamba1@hawk.iit.edu>
2016-11-04 01:15:20 +0000

    made small changes

--- installapp.sh
+++ installapp.sh
@@ -2,19 +2,10 @@
 
 echo "hello" > /home/ubuntu/hello.txt
  sudo apt-get update -y
- sudo apt-get install -y apache2
- sudo systemctl enable apache2
- sudo systemctl start apache2
+ sudo apt-get install -y git apache2 mysql-server php libapache2-mod-php curl php-curl zip unzip 
 
- sudo apt-get -y install mysql-server 
- sudo apt-get -y install php libapache2-mod-php
- sudo /etc/init.d/apache2 restart
- php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
- sudo apt-get install -y curl
- sudo apt-get install -y php-curl
- sudo apt-get install -y zip
- sudo apt-get install -y unzip
- sudo apt-get install -y git
+sudo /etc/init.d/apache2 restart
+php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
 
  curl -sS https://getcomposer.org/installer | php
  php composer.phar require aws/aws-sdk-php
