7ab2d448e326b41dc1dba459eee7a2b441ae7909
jlamba1@hawk.iit.edu <jlamba1@hawk.iit.edu>
2016-11-08 15:35:42 +0000

    updated break

--- dbtest.php
+++ dbtest.php
@@ -1,22 +1,22 @@
 <?php
 require 'vendor/autoload.php';
-echo "begin database";
+echo "begin database<br />";
 $link = mysqli_connect("jl-instance1.cjuyoiserrk7.us-west-2.rds.amazonaws.com:3306","jatindb","Jlamba1db","school") or die("Error " . mysqli_error($link));
 /* check connection */
 if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+printf("Connect failed: %s <br/>", mysqli_connect_error());
+exit();
 }
 $create_table = 'CREATE TABLE IF NOT EXISTS students
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    Name VARCHAR(255),
-    Age INT(3),
-    PRIMARY KEY(id)
+id INT NOT NULL AUTO_INCREMENT,
+Name VARCHAR(255),
+Age INT(3),
+PRIMARY KEY(id)
 )';
 $create_tbl = $link->query($create_table);
 if ($create_table) {
-	echo "Table is created or No error returned.";
+	echo "Table is created or No error returned.<br />";
 }
 else {
         echo "error!!";  
@@ -28,16 +28,16 @@ if (!($stmt = $link->prepare("INSERT INTO students (Name, Age) VALUES ('Jatin', 
 if (!$stmt->execute()) {
     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 }
-printf("%d Row inserted.\n", $stmt->affected_rows);
+printf("%d Row inserted.<br />", $stmt->affected_rows);
 /* explicit close recommended */
 $stmt->close();
 $link->real_query("SELECT * FROM students");
 $res = $link->use_result();
-echo "Result set order...\n";
+echo "Result set order...<br />";
 while ($row = $res->fetch_assoc()) {
-    echo " Id = " . $row['id'] . "\n";
-	echo " Name = " . $row['Name'] . "\n";
-	echo " Age = " . $row['Age'] . "\n";
+    echo " Id = " . $row['id'] . "<br />";
+	echo " Name = " . $row['Name'] . "<br />";
+	echo " Age = " . $row['Age'] . "<br />";
 }
 $link->close();
 ?>
