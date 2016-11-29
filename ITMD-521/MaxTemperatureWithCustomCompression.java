1f159347ee37c926482803c219a3ced18eaa2325
Jatin Lamba <jatinlamba@users.noreply.github.com>
2016-11-28 22:58:33 -0600

    Update MaxTemperatureWithCustomCompression.java

--- ITMD-521/MaxTemperatureWithCustomCompression.java
+++ ITMD-521/MaxTemperatureWithCustomCompression.java
@@ -40,7 +40,7 @@ public class MaxTemperatureWithCustomCompression {
     job.setMapperClass(MaxTemperatureMapper.class);
     job.setCombinerClass(MaxTemperatureReducer.class);
     job.setReducerClass(MaxTemperatureReducer.class);
-    job.setNumReduceTasks(1);   
+    job.setNumReduceTasks(2);   
     
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
