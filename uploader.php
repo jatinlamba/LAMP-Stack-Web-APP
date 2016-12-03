4d2bdcbdba95b9077e7f13d77978195f63986d49
Ubuntu <jlamba1@hawk.iit.edu>
2016-12-03 10:27:13 +0000

    CSS added

--- uploader.php
+++ uploader.php
@@ -1,5 +1,26 @@
 
 
+<html>
+<head>
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css">
+<title>uploader page</title>
+<style>
+    body {
+        color: navy !important;;
+        background-color: lightblue !important;;
+        font-family: "Comic Sans MS", cursive, sans-serif ;
+    }
+    </style>
+
+</head>
+<body>
+<div id="main">
+<h1>Uploaded </h1>
+<hr />
+<a href="gallery.php"> Gallery </a> | <a href="upload.php"> Upload </a>
+</body>
+
 <?php
 
 session_start();
@@ -10,9 +31,7 @@ $s3 = new Aws\S3\S3Client([
     'region'  => 'us-west-2'
 ]);
 
-
-// have to hard code this here because index.php doesn't exist
-echo "\n" . $_SESSION['username'] ."\n";
+echo "<br />" . $_SESSION['username'] ."<br />";
 
 // Retrieve the POSTED file information (location, name, etc, etc)
 
@@ -21,14 +40,10 @@ $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
 #echo '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
+    echo "<br /> File is valid, and was successfully uploaded. <br />";
 } else {
     echo "Possible file upload attack!\n";
 }
-
-echo 'Here is some more debugging info:';
-print_r($_FILES);
-
 
 // Upload file to S3 bucket
 $s3result = $s3->putObject([
@@ -42,7 +57,7 @@ $s3result = $s3->putObject([
 ]);
 
 $url=$s3result['ObjectURL'];
-echo "\n". "This is your URL: " . $url ."\n";
+echo "<br />". "This is your URL: " . $url ."<br />";
 /* create connection */
 $link = mysqli_connect("jl-instance1.cjuyoiserrk7.us-west-2.rds.amazonaws.com","jatindb","Jlamba1db","school",3306) or die("Error " . mysqli_error($link));
 
@@ -73,7 +88,7 @@ if (!$stmt->execute()) {
     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 }
 
-printf("%d Row inserted.\n", $stmt->affected_rows);
+printf("<br /> %d Row inserted. <br />", $stmt->affected_rows);
 
 
 /* explicit close recommended */
